1d2fc18bf904d73702c67f7248a25f32a4cb80eb
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-11 19:19:28 +0630

    render JSX component at designed place by using react-dom portal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,24 +2,49 @@ import React from "react";
 import Card from "./Card.";
 import Button from "./Button";
 
+import ReactDOM from "react-dom";
+
 import styles from "./ErrorModal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+// ReactDOM portal လုပ်နည်းက တံခါးပေါက်(portal)၊ ပို့မဲ့ကောင်(JSX component)၊ ပို့မဲ့နေရာ(public html DOM) ရှိရမယ်
 
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
-      <div className={styles.backdrop} onClick={props.onConfirm}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-
-        <footer className={styles.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClick={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
