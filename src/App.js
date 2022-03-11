4eede1160888152a6cd30a2376d3ea022e5e16a4
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-11 12:18:04 +0630

    add userList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
+
+const USER_LIST = [];
 
 function App() {
+  const [userList, setUserList] = useState(USER_LIST);
+  const addUserHandler = (newUser) => {
+    setUserList((prev) => {
+      const pervState = [...prev];
+      pervState.unshift(newUser);
+      return pervState;
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card.";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    // Validate empty input
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0)
+      return;
+
+    // Validate 0 and negative age
+    // + to change integer
+    if (+enteredAge < 1) return;
+    const newUser = {
+      name: enteredName,
+      age: enteredAge,
+    };
+    props.onAddUser(newUser);
+    setEnteredAge("");
+    setEnteredName("");
   };
+
+  const nameChangeHandaler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const ageChangeHandaler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          value={enteredName}
+          onChange={nameChangeHandaler}
+        ></input>
         <label htmlFor="userage">Age(years)</label>
-        <input id="userage" type="number"></input>
+        <input
+          id="userage"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandaler}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
